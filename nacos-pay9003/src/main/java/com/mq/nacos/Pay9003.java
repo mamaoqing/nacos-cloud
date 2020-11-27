937b5828c9cf2aa0cd4bc1de5a29f035cae7b0a7
mamaoqing <luyuna1121@163.com>
2020-11-27 14:18:28 +0800

    修改不提交文件

---
+++ nacos-pay9003/src/main/java/com/mq/nacos/Pay9003.java
@@ -0,0 +1,22 @@
+package com.mq.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author mq
+ * @description: TODO
+ * @title: Pay9001
+ * @projectName nacos-cloud
+ * @date 2020/11/2616:47
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+public class Pay9003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pay9003.class,args);
+    }
+}
---
+++ nacos-pay9003/src/main/java/com/mq/nacos/controller/PayController.java
@@ -0,0 +1,27 @@
+package com.mq.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mq
+ * @description: TODO
+ * @title: PayController
+ * @projectName nacos-cloud
+ * @date 2020/11/2616:36
+ */
+@RestController
+@RequestMapping("/pay/")
+public class PayController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("{id}")
+    public String pay(@PathVariable("id") String id){
+        return "访问端口号是<"+port+">的服务,获取到的id内容是："+ id;
+    }
+}
